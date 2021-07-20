f7270ace6e335db2b580aaa3cb1c02175c958ac2
AlanPatrik <alan.santos@ilabqa.com>
2021-07-20 10:48:31 -0300

    first commit

---
+++ src/main/java/com/alanpatrik/mongodbapplication/MongodbApplication.java
@@ -0,0 +1,13 @@
+package com.alanpatrik.mongodbapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbApplication.class, args);
+	}
+
+}
