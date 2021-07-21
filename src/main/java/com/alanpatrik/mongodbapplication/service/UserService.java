8f6a2fe9c6c2edf8854003080efef44160c591eb
AlanPatrik <alan.santos@ilabqa.com>
2021-07-20 21:28:49 -0300

    getting a user by id

--- src/main/java/com/alanpatrik/mongodbapplication/service/UserService.java
+++ src/main/java/com/alanpatrik/mongodbapplication/service/UserService.java
@@ -1,20 +1,27 @@
 package com.alanpatrik.mongodbapplication.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alanpatrik.mongodbapplication.domain.User;
 import com.alanpatrik.mongodbapplication.repository.UserRepository;
+import com.alanpatrik.mongodbapplication.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
-	private UserRepository repository;
+	private UserRepository repo;
 	
 	public List<User> findAll() {
-		return repository.findAll();
+		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
